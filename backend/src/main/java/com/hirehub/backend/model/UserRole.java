5501a4e8e81460c77f3027f21e83ec383255041f
Raviraj00001 <ravirajsingh123567@gmail.com>
2026-04-18 23:34:10 +0530

    Deploy frontend dist to GitHub Pages

--- backend/src/main/java/com/hirehub/backend/model/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.hirehub.backend.model;
-
-public enum UserRole {
-    JOB_SEEKER,
-    EMPLOYER
-}
--- backend/src/main/java/com/hirehub/backend/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hirehub.backend.repository;
-
-import com.hirehub.backend.model.Job;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, Long> {
-    List<Job> findByEmployerId(Long employerId);
-}
